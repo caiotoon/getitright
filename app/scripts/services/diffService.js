ddcdb34e10440b2c7fa0919973402f539d8dab88
Caio Cunha <caio@caiotoon.com>
2014-06-21 18:58:22 -0400

    feat(diff): created diff service
    Created diff service and updated `karma.conf.js` to include new files in the correct order.

---
+++ app/scripts/services/diffService.js
@@ -0,0 +1,52 @@
+/**
+ * @ngdoc service
+ * @name diff
+ * @kind class
+ *
+ * @description
+ * Computes and returns a string representing the difference between two given
+ * strings.
+ * Words not in the original string will be shown in between <ins> tags, and
+ * words not in the matching string will be shown in between <del> tags.
+ */
+angular.module('getitright')
+
+  // our diff lib exposes this global funciton
+  .value('diffImpl', JsDiff)
+
+  // then we inject it here
+  .provider('diff', function() {
+    var _excessTpl, _missingTpl, _matchingTpl;
+
+    this.setTemplates = function(excessTpl, missingTpl, matchingTpl) {
+      _excessTpl = excessTpl;
+      _missingTpl = missingTpl;
+      _matchingTpl = matchingTpl;
+    };
+
+    this.$get = ['diffImpl', function(diffImpl) {
+      return {
+        isPhrase: function(text) {
+          return text.indexOf(' ') > -1;
+        }, compare: function(original, matching) {
+          var diff, str;
+
+          if (this.isPhrase(original)) {
+            diff = diffImpl.diffWords(original, matching);
+          } else {
+            diff = diffImpl.diffChars(original, matching);
+          }
+
+          str = '';
+          diff.forEach(function formatPart(part) {
+            var tpl = part.added ? _excessTpl :
+              part.removed ? _missingTpl : _matchingTpl;
+
+            str += tpl.replace('%s', part.value);
+          });
+
+          return str;
+        }
+      };
+    }];
+  });
